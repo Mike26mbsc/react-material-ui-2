8de521970bad3325eaaed2e780adbd05131ba8ea
Michael Farrington <mike26mbsc@gmail.com>
2019-08-15 14:51:45 -0400

    NavBar.JS initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/NavBar';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div>
-    
+      <NavBar/>
     </div>
   );
 }
